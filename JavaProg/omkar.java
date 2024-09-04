54768bb9c15243c97c6317413ff0713afced117e
omijbhabha <omkarsingh5128@gmail.com>
2024-09-04 17:37:43 +0530

    created file

---
+++ JavaProg/omkar.java
@@ -0,0 +1,12 @@
+class Vehicle {
+    int mpg;
+    int maxSpeed;
+}
+
+public class omkar {
+    public static void main(String[] args) {
+        Vehicle alto = new Vehicle();
+        alto.mpg = 67;
+        System.out.println("Alto MPG: " + alto.mpg);
+    }
+}
